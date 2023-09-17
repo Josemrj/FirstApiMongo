692bd6e1b3b3a4edee935a8ee4c310202144d186
José Eduardo Jesus <jose.martins006@outlook.com>
2023-09-16 22:31:30 -0300

    Create MongoDB.cs for manage the connection to MongoDB and register the FirstApiMongo.Data.MongoDB class as a singleton service in the dependency injection container.

---
+++ Data/MongoDB.cs
@@ -0,0 +1,44 @@
+﻿using FirstApiMongo.Data.Collections;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace FirstApiMongo.Data
+{
+    public class MongoDB
+    {
+        public IMongoDatabase Database { get; }
+
+        public MongoDB(IConfiguration configuration)
+        {
+            try
+            {
+                var settings = MongoClientSettings.FromUrl(new MongoUrl(configuration["ConnectionString"]));
+                var client = new MongoClient(settings);
+
+                Database = client.GetDatabase(configuration["NomeBanco"]);
+                //MapClasses();
+            }
+            catch (Exception ex)
+            {
+                throw new MongoException("It was not possible to connect to MongoDB", ex);
+            }
+        }
+
+        public void MapClasses()
+        {
+            var conventionPack = new ConventionPack { new CamelCaseElementNameConvention() };
+
+            ConventionRegistry.Register("camelCase", conventionPack, t => true);
+
+            if (!BsonClassMap.IsClassMapRegistered(typeof(Infectado)))
+            {
+                BsonClassMap.RegisterClassMap<Infectado>(i =>
+                {
+                    i.AutoMap();
+                    i.SetIgnoreExtraElements(true);
+                });
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<FirstApiMongo.Data.MongoDB>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
