446334c7a87eb1df2c7d7d2a131a0466021e5c2b
José Eduardo Jesus <jose.martins006@outlook.com>
2023-09-16 23:31:45 -0300

    Add InfectadoController.cs

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,41 @@
+﻿using FirstApiMongo.Data.Collections;
+using FirstApiMongo.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace FirstApiMongo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.Database.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adcionado com sucesso");
+
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectado()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+    }
+}
